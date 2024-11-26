0edc8af52bca73274c8eff3bee726b47febd21c7
Seojun <auddml321@hanyang.ac.kr>
2024-11-26 22:16:04 +0900

    BelogRepository 생성

---
+++ src/main/java/hython/secret/Repository/BelogRepository.java
@@ -0,0 +1,8 @@
+package hython.secret.Repository;
+
+import hython.secret.Entity.Belog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BelogRepository extends JpaRepository<Belog, Integer> {
+
+}
