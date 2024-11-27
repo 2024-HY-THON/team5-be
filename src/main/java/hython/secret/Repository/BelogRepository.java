61f927bf6fb79009ebfa7abea9943ea7a4cfa116
Seojun <auddml321@hanyang.ac.kr>
2024-11-27 20:48:38 +0900

    #feat BelogRepository 생성 findByUserId 추가

--- src/main/java/hython/secret/Repository/BelogRepository.java
+++ src/main/java/hython/secret/Repository/BelogRepository.java
@@ -1,8 +1,9 @@
 package hython.secret.Repository;
 
+import java.util.*;
 import hython.secret.Entity.Belog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BelogRepository extends JpaRepository<Belog, Integer> {
-
+    List<Belog> findByUserId(int userId);
 }
